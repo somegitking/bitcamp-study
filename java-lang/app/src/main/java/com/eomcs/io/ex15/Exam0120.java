a7d84bfeeac96695c6605e4ee69831f20194a088
somegitking <ironvegapentech@gmail.com>
2022-02-22 19:51:11 +0900

    .

--- java-lang/app/src/main/java/com/eomcs/io/ex15/Exam0120.java
+++ java-lang/app/src/main/java/com/eomcs/io/ex15/Exam0120.java
@@ -1,30 +1,20 @@
 // 바이너리 파일을 텍스트 형식으로 저장하기 
 package com.eomcs.io.ex15;
 
-import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.util.Base64;
-import java.util.Base64.Decoder;
+import java.util.StringTokenizer;
 
 public class Exam0120 {
-  public static void main(String[] args) throws Exception {
+  public static void main(String[] args) {
 
-    Decoder decoder = Base64.getDecoder();
 
-    FileReader in = new FileReader("./temp/photo.txt");
-    FileOutputStream out = new FileOutputStream("./temp/photox.jfif");
+    StringTokenizer st = new StringTokenizer("as d fas df", " ");
 
-    char[] buf = new char[1000000];
-    int len = in.read(buf);
-    System.out.printf("읽은 문자 수: %d\n", len);
+    System.out.println(st.nextToken());
+    System.out.println(st.nextToken());
+    System.out.println(st.nextToken());
+    System.out.println(st.nextToken());
 
-    // 문자 배열에 저장된 Base64 텍스트를 바이너리 데이터로 변환하기
-    byte[] decodedBytes = decoder.decode(String.valueOf(buf, 0, len));
 
-    // 텍스트로 변환된 데이터를 파일로 출력하기
-    out.write(decodedBytes);
 
-    in.close();
-    out.close();
   }
 }
